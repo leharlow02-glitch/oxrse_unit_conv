2ff837b4d61080c99d8166a699e7b069e6d96cfa
An Mei Daniels <amybda2004@outlook.com>
2025-10-16 16:23:18 +0100

    Added more forgiveness with SI unit names

--- src/oxrse_unit_conv/si.py
+++ src/oxrse_unit_conv/si.py
@@ -9,25 +9,38 @@ from meta import classes
 
 second = classes.SIUnit("second", "s")
 s = second
+seconds = second
 
 meter = classes.SIUnit("meter", "m")
 m = meter
+meters = meter
+
 meter_sq = classes.SIUnit("meter_sq", "m", 2)
 m2 = meter_sq
+meter_squared = meter_sq
+meters_squared = meter_sq
+
 meter_cu = classes.SIUnit("meter_cu", "m", 3)
 m3 = meter_cu
+meter_cubed = meter_cu
+meters_cubed = meter_cu
 
 kilogram = classes.SIUnit("kilogram", "kg")
 kg = kilogram
+kgs = kilogram
+kilograms = kilogram
 
 ampere = classes.SIUnit("ampere", "A")
 A = ampere
+amperes = ampere
 
 kelvin = classes.SIUnit("kelvin", "K")
 K = kelvin
 
 mole = classes.SIUnit("mole", "mol")
 mol = mole
+mols = mole
+moles = mole
 
 candela = classes.SIUnit("candela", "cd")
 cd = candela
